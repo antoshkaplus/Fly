a8bf5150fe93eb7a8ce63ca1821007a6cb2cc860
Anton Logunov <antoshkaplus@gmail.com>
2017-04-26 20:14:56 -0400

    add backend ResourceInteger

---
+++ bee/src/main/java/com/antoshkaplus/bee/backend/ResourceInteger.java
@@ -0,0 +1,24 @@
+package com.antoshkaplus.bee.backend;
+
+/**
+ * Created by antoshkaplus on 4/26/17.
+ */
+
+public class ResourceInteger {
+
+    public int value;
+
+    public ResourceInteger() {}
+
+    public ResourceInteger(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
